2443d1c09ea9711a4f6e191c3d59dacc3b2812c6
Hiraku <dev@hiraku.tw>
2022-02-16 18:51:05 +0800

    Update shopee checkin and luckydraw

--- scripts/shopee_checkin.js
+++ scripts/shopee_checkin.js
@@ -1,4 +1,4 @@
-var refreshURL = {
+const refershRequest = {
   url: 'https://mall.shopee.tw/api/v4/client/refresh',
   headers: {
     Cookie:
@@ -6,7 +6,7 @@ var refreshURL = {
   },
 };
 
-var accountInfoURL = {
+const accountInfoRequest = {
   url: 'https://shopee.tw/api/v2/user/account_info?from_wallet=false&skip_address=1&need_cart=1',
   headers: {
     Cookie:
@@ -15,7 +15,7 @@ var accountInfoURL = {
   },
 };
 
-var checkinURL = {
+const checkinRequest = {
   url: 'https://shopee.tw/mkt/coins/api/v2/checkin',
   headers: {
     'Cookie': $persistentStore.read("CookieSP") + ';SPC_EC=' + $persistentStore.read("SPC_EC") + ';',
@@ -24,35 +24,33 @@ var checkinURL = {
 };
 
 function updateSPC_EC() {
-  $httpClient.get(refreshURL, function (error, response, data) {
+  $httpClient.get(refershRequest, function (error, response, data) {
     if (error) {
       $notification.post('蝦皮 SPC_EC Cookie', '', '連線錯誤‼️');
-      $done();
     } else {
       if (response.status == 200) {
-        let cookie = $persistentStore.write(response.headers['Set-Cookie'].split('SPC_EC=')[1].split(';')[0], 'SPC_EC');
+        const cookie = $persistentStore.write(response.headers['Set-Cookie'].split('SPC_EC=')[1].split(';')[0], 'SPC_EC');
         if (cookie) {
           // $notification.post('蝦皮 SPC_EC 保存成功🎉', '', '');
           updateCookie();
         } else {
           $notification.post('蝦皮 SPC_EC Cookie 保存失敗‼️', '', '請重新登入');
         }
-        $done();
       } else {
         $notification.post('蝦皮 SPC_EC Cookie 保存失敗‼️', '', '請重新登入');
       }
     }
+    $done();
   });
 }
 
 function updateCookie() {
-  $httpClient.get(accountInfoURL, function (error, response, data) {
+  $httpClient.get(accountInfoRequest, function (error, response, data) {
     if (error) {
       $notification.post('蝦皮 Cookie 保存失敗‼️', '', '連線錯誤‼️');
-      $done();
     } else {
       if (response.status == 200) {
-        let cookie = $persistentStore.write(
+        const cookie = $persistentStore.write(
           response.headers['Set-Cookie'],
           'CookieSP'
         );
@@ -62,38 +60,34 @@ function updateCookie() {
         } else {
           $notification.post('蝦皮 Cookie 保存失敗‼️', '', '請重新登入');
         }
-        $done();
       } else {
         $notification.post('蝦皮 Cookie 保存失敗‼️', '', '請重新登入');
       }
+      $done();
     }
   });
 }
 
 function checkin() {
-  $httpClient.post(checkinURL, function (error, response, data) {
+  $httpClient.post(checkinRequest, function (error, response, data) {
     if (error) {
       $notification.post("蝦皮簽到失敗‼️", "", "連線錯誤‼️")
-      $done();
     } else {
       if (response.status == 200) {
-        let obj = JSON.parse(data);
+        const obj = JSON.parse(data);
         if (obj["data"]["success"]) {
-          // var user = obj["data"]["username"];
-          var coins = obj["data"]["increase_coins"];
-          var checkinday = obj["data"]["check_in_day"];
+          const coins = obj["data"]["increase_coins"];
+          const checkinday = obj["data"]["check_in_day"];
           $notification.post("蝦皮已連續簽到 " + checkinday + " 天", "", "今日已領取 " + coins + "💰💰💰");
-          $done();
         }
         else {
           $notification.post("蝦皮簽到失敗‼️", "", "本日已簽到‼️");
         }
-        $done();
       } else {
         $notification.post("蝦皮 Cookie 已過期‼️", "", "請重新登入 🔓");
-        $done();
       }
     }
+    $done();
   });
 }
 
--- scripts/shopee_luckydraw.js
+++ scripts/shopee_luckydraw.js
@@ -1,44 +1,35 @@
-var shopeeluckydrawUrl = {
-    url: 'https://games.shopee.tw/luckydraw/api/v1/lucky/event/0244d69e637bbb73',
-    headers: {
-        'Cookie': $persistentStore.read("CookieSP") + ';SPC_EC=' + $persistentStore.read("SPC_EC") + ';',
-        'X-CSRFToken': $persistentStore.read("CSRFTokenSP"),
-    },
-    body: {
-        "request_id": (Math.random() * 10 ** 20).toFixed(0).substring(0, 16),
-        "app_id": "E9VFyxwmtgjnCR8uhL",
-        "activity_code": "010ac47631cf4bb5",
-        "source": 0
-    },
-}
+var request = {
+  url: "https://games.shopee.tw/luckydraw/api/v1/lucky/event/a3267155f3ec89c2",
+  headers: {
+    'Cookie': $persistentStore.read("CookieSP") + ';SPC_EC=' + $persistentStore.read("SPC_EC") + ';',
+    'X-CSRFToken': $persistentStore.read("CSRFTokenSP"),
+  },
+  body: {
+    request_id: (Math.random() * 10 ** 20).toFixed(0).substring(0, 16),
+    app_id: "E9VFyxwmtgjnCR8uhL",
+    activity_code: "e37b7dec5976a29c",
+    source: 0,
+  },
+};
 
-$httpClient.post(shopeeluckydrawUrl, function (error, response, data) {
-    if (error) {
-        $notification.post("蝦幣寶箱", "", "連線錯誤‼️")
-        $done();
+$httpClient.post(request, function (error, response, data) {
+  if (error) {
+    $notification.post("🍤 蝦幣寶箱", "", "連線錯誤‼️");
+  } else {
+    if (response.status == 200) {
+      const obj = JSON.parse(data);
+      if (obj["msg"] == "no chance") {
+        $notification.post("🍤 今日已領過蝦幣寶箱", "", "每日只能領一次‼️");
+      } else if (obj["msg"] == "success") {
+        const packagename = obj["data"]["package_name"];
+        $notification.post("🍤 蝦幣寶箱領取成功 ✅", "", "獲得 👉 " + packagename + " 💎"
+        );
+      } else if (obj["msg"] == "expired" || obj["msg"] == "event already end") {
+        $notification.post("🍤 蝦幣寶箱活動已過期 ❌", "", "請嘗試更新模組或腳本，或等待作者更新‼️");
+      }
+    } else {
+      $notification.post("🍤 蝦皮 Cookie 已過期‼️", "", "請重新抓取 🔓");
     }
-
-    else {
-        if (response.status == 200) {
-            let obj = JSON.parse(data);
-            if (obj["msg"] == 'no chance') {
-                $notification.post("今日已領過蝦皮寶箱，每日只能領一次‼️", "", "");
-                $done();
-            }
-            else if (obj["msg"] == 'success') {
-                var packagename = obj["data"]["package_name"];
-                $notification.post("恭喜獲得蝦幣寶箱：" + packagename, "", "");
-                $done();
-            }
-            else if (obj["msg"] == 'expired') {
-                $notification.post("活動已過期，請嘗試更新模組或腳本，或等待作者更新", "", "");
-                $done();
-            }
-            $done();
-        }
-        else {
-            $notification.post("蝦皮 Cookie 已過期‼️", "", "請重新登入 🔓");
-            $done();
-        }
-    }
+  }
+  $done();
 });
