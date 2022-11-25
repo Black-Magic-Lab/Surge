0436a7bfddd5d1c82e72380d5856c7b7c399adee
Hiraku <dev@hiraku.tw>
2022-11-25 14:11:01 +0800

    update iPhone check module

--- scripts/iphone_check_store.js
+++ scripts/iphone_check_store.js
@@ -1,43 +1,53 @@
 const debug = false;
-const model = $persistentStore.read("iPhoneModel")
+const model = $persistentStore.read('iPhoneModel');
 
 var checkRequest = {
-    url: '',
-    headers: {
-        'x-ma-pcmh': 'REL-5.14.0',
-        'X-DeviceConfiguration': 'ss=3.00;dim=1170x2532;m=iPhone;v=iPhone14,2;vv=5.14;sv=15.0.1'
-    },
+  url: '',
+  headers: {
+    'x-ma-pcmh': 'REL-5.17.0',
+    'X-DeviceConfiguration': 'ss=3.00;dim=1170x2532;m=iPhone;v=iPhone14,2;vv=5.17;sv=15.3'
+  },
 }
 
 
 const stores = ['R713', 'R694'];
 
-for (const store of stores) {
-    checkStore(model, store);
+function checkStore(model, index) {
+  const store = stores[index];
+  checkRequest['url'] = 'https://mobileapp.apple.com/merch/p/tw/pickup/quote/' + model + '?partNumber=' + model + '&store=' + store;
+  $httpClient.get(checkRequest, function (error, response, data) {
+    if (error) {
+      $notification.post('🍎 直營店庫存檢查失敗 ❌', '', '連線錯誤‼️')
+    } else {
+      if (response.status == 200) {
+        let obj = JSON.parse(data);
+        if (obj.availabilityStatus != 'NOT_AVAILABLE') {
+          $notification.post('📱 ' + model + ' 有貨', '', obj.pickupQuote);
+        } 
+        else {
+          console.log('📱 ' + obj.storeNumber + ' ' + obj.pickupQuote);
+        }
+        if (index < stores.length - 1) {
+          checkStore(model, index + 1);
+        }
+        else {
+          $done();
+        }
+      } else {
+        $notification.post('🍎 直營店庫存檢查失敗 ❌', '', '連線錯誤‼️');
+      }
+    }
+  });
 }
 
-function checkStore(model, store) {
-    checkRequest['url'] = 'https://mobileapp.apple.com/merch/p/tw/pickup/quote/' + model + '?partNumber=' + model + '&store=' + store;
-    $httpClient.get(checkRequest, function (error, response, data) {
-        if (error) {
-            $notification.post("🍎 直營店庫存檢查失敗 ❌", "", "連線錯誤‼️")
-            $done();
-        } else {
-            if (response.status == 200) {
-                let obj = JSON.parse(data);
-                if (obj["availabilityStatus"] != 'NOT_AVAILABLE') {
-                    $notification.post("📱 " + model + " 有貨", "", obj["pickupQuote"]);
-                    $done();
-                }
-                else if (debug) {
-                    $notification.post("📱 " + obj["pickupQuote"], "", "");
-                }
-
-                $done();
-            } else {
-                $notification.post("🍎 直營店庫存檢查失敗 ❌", "", "連線錯誤‼️");
-                $done();
-            }
-        }
-    });
+if (Date.now() < 1663286400000) {
+  console.log('還沒開賣');
+  $done();
+}
+else if (new Date().getHours() < 8 || new Date().getHours() > 21) {
+  console.log('不在營業時間');
+  $done();
+}
+else {
+  checkStore(model, 0);
 }
