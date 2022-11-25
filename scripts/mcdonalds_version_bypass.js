c1350387bcbc4d6159965e5cead881ee1e443a73
Hiraku <dev@hiraku.tw>
2022-11-25 14:11:22 +0800

    update mcdonalds spoof version

--- scripts/mcdonalds_version_bypass.js
+++ scripts/mcdonalds_version_bypass.js
@@ -1,5 +1,15 @@
+const version = '3.3.1';
+const ua = 'Mcdonalds/3.3.1';
 
-$request.headers['version'] = '3.9.9';
-$request.headers['appVersion'] = '3.9.9';
+$request.headers['version'] = version;
+
+if ($request.headers['appversion']) {
+	$request.headers['appversion'] = version; //HTTP2
+	$request.headers['user-agent'] = $request.headers['user-agent'].replace(new RegExp('Mcdonalds/3.[0-9].[0-9]'), ua);
+}
+else {
+	$request.headers['appVersion'] = version;
+	$request.headers['User-Agent'] = $request.headers['User-Agent'].replace(new RegExp('Mcdonalds/3.[0-9].[0-9]'), ua);
+}
 
 $done($request);
