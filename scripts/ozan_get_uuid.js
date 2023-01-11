3adcb6e622c0d0801e6cdfddbdaa88bf8f851e69
Hiraku <dev@hiraku.tw>
2023-01-11 12:56:51 +0800

    Ozan support loon

--- scripts/ozan_get_uuid.js
+++ scripts/ozan_get_uuid.js
@@ -2,6 +2,10 @@ function ozanNotify(subtitle = '', message = '') {
   $notification.post('Ozan 取得 UUID', subtitle, message);
 };
 
+function isLoon() {
+  return 'undefined' !== typeof $loon;
+}
+
 const uuid = $request.headers['X-DEVICE-CODE'] || $request.headers['x-device-code'];
 if (uuid) {
   let writeData = $persistentStore.write(uuid, 'OzanUUID');
@@ -11,7 +15,7 @@ if (uuid) {
       '請稍後嘗試'
     );
   } else {
-    if ('undefined' !== typeof $loon) {
+    if (isLoon()) {
       ozanNotify(
         'UUID 保存成功 ✅',
         '請到「腳本」→「數據持久化」→「讀取指定數據」，輸入 OzanUUID，然後複製所有內容'
