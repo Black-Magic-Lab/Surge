14b5174c8fcade064a50e0b3b9f56b987fc29999
Hiraku <dev@hiraku.tw>
2023-01-31 00:56:32 +0800

    new data structure

--- scripts/mcdonalds_set_token.js
+++ scripts/mcdonalds_set_token.js
@@ -1,12 +1,59 @@
 const token = '改掉這一行';
 
 //================================================================
-function mcdonaldsNotify(subtitle = '', message = '') {
+let showNotification = true;
+
+function surgeNotify(subtitle = '', message = '') {
   $notification.post('🍟 麥當勞 token', subtitle, message, { 'url': 'mcdonalds.app://' });
 };
-$persistentStore.write(token, "McdonaldsToken");
-mcdonaldsNotify(
-  '設定完成 ✅',
-  ''
-)
-$done({})
+
+function handleError(error) {
+  if (Array.isArray(error)) {
+    console.log(`❌ ${error[0]} ${error[1]}`);
+    if (showNotification) {
+      surgeNotify(error[0], error[1]);
+    }
+  } else {
+    console.log(`❌ ${error}`);
+    if (showNotification) {
+      surgeNotify(error);
+    }
+  }
+}
+
+function getSaveObject(key) {
+  const string = $persistentStore.read(key);
+  return !string || string.length === 0 ? {} : JSON.parse(string);
+}
+
+async function saveToken() {
+  return new Promise((resolve, reject) => {
+    try {
+      if (token.length === 0 || token === '改掉這一行') {
+        return reject(['保存失敗 ‼️', '請先設定 token']);
+      }
+      let mcdonaldsInfo = getSaveObject('McdonaldsInfo');
+      mcdonaldsInfo.token = token;
+      const save = $persistentStore.write(JSON.stringify(mcdonaldsInfo, null, 4), 'McdonaldsInfo');
+      if (!save) {
+        return reject(['保存失敗 ‼️', '無法儲存 token']);
+      } else {
+        return resolve();
+      }
+    } catch (error) {
+      return reject(['保存失敗 ‼️', error]);
+    }
+  });
+}
+
+(async () => {
+  console.log('ℹ️ 麥當勞設定 token v20230125.1');
+  try {
+    await saveToken();
+    console.log('✅ token 保存成功');
+    surgeNotify('保存成功 ✅', '');
+  } catch (error) {
+    handleError(error);
+  }
+  $done({});
+})();
--- scripts/mcdonalds_token.js
+++ scripts/mcdonalds_token.js
@@ -1,28 +1,70 @@
-function mcdonaldsNotify(subtitle = '', message = '') {
+let showNotification = true;
+const fakeAppVersion = '3.9.9';
+
+function surgeNotify(subtitle = '', message = '') {
   $notification.post('🍟 麥當勞 token', subtitle, message, { 'url': 'mcdonalds.app://' });
 };
 
-const accessToken = $request.headers['accessToken'] || $request.headers['accesstoken'];
-if (accessToken) {
-  let cookie = $persistentStore.write(accessToken, "McdonaldsToken");
-  if (!cookie) {
-    mcdonaldsNotify(
-      '保存失敗 ‼️',
-      '請稍後嘗試'
-    );
+function handleError(error) {
+  if (Array.isArray(error)) {
+    console.log(`❌ ${error[0]} ${error[1]}`);
+    if (showNotification) {
+      surgeNotify(error[0], error[1]);
+    }
   } else {
-    mcdonaldsNotify(
-      '保存成功 🍪',
-      ''
-    );
+    console.log(`❌ ${error}`);
+    if (showNotification) {
+      surgeNotify(error);
+    }
   }
-} else {
-  mcdonaldsNotify(
-    '保存失敗 ‼️',
-    '請重新登入'
-  );
 }
 
-$request.headers['version'] = '3.9.9';
-$request.headers['appVersion'] = '3.9.9';
-$done($request);
+function getSaveObject(key) {
+  const string = $persistentStore.read(key);
+  return !string || string.length === 0 ? {} : JSON.parse(string);
+}
+
+function isManualRun(checkRequest = false, checkResponse = false) {
+  if (checkRequest) {
+    return typeof $request === 'undefined' || ($request.body && JSON.parse($request.body).foo === 'bar');
+  }
+  if (checkResponse) {
+    return typeof $response === 'undefined' || ($response.body && JSON.parse($response.body).foo === 'bar');
+  }
+  return false;
+}
+
+async function getToken() {
+  return new Promise((resolve, reject) => {
+    try {
+      const token = $request.headers['accessToken'] || $request.headers['accesstoken'];
+      let mcdonaldsInfo = getSaveObject('McdonaldsInfo');
+      mcdonaldsInfo.token = token;
+      const save = $persistentStore.write(JSON.stringify(mcdonaldsInfo, null, 4), 'McdonaldsInfo');
+      if (!save) {
+        return reject(['保存失敗 ‼️', '無法儲存 token']);
+      } else {
+        return resolve();
+      }
+    } catch (error) {
+      return reject(['保存失敗 ‼️', error]);
+    }
+  });
+}
+
+(async () => {
+  console.log('ℹ️ 麥當勞自動儲存 token v20230125.1');
+  try {
+    if (isManualRun(true, false)) {
+      throw '請勿手動執行此腳本';
+    }
+    await getToken();
+    console.log('✅ token 保存成功');
+    surgeNotify('保存成功 ✅', '');
+  } catch (error) {
+    handleError(error);
+  }
+  $request.headers['version'] = fakeAppVersion;
+  $request.headers['appVersion'] = fakeAppVersion;
+  $done($request);
+})();
