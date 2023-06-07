e892233e8355ab6cf6dc184e3ae5cb512695dbea
Hiraku <dev@hiraku.tw>
2023-06-07 18:03:52 +0800

    support loon

--- scripts/surge_check.js
+++ scripts/surge_check.js
@@ -1,10 +1,11 @@
+const foo = 'bar'; //白痴 loon 設計，少任意一行垃圾 code 就不會執行下面的腳本
+
 (() => {
   console.log('ℹ️ Surge 功能檢查 v20230115.1');
   console.log('⚠️ 請勿手動執行此腳本');
   if ('undefined' !== typeof $response) {
     let body = $response.body;
     $response.body = body.replace('id="mitm-resule">Failed ❌', 'id="mitm-resule">Success ✅');
-
     if ('undefined' !== typeof $loon) {
       body = $response.body;
       $response.body = body.replace('<h1>Surge Check</h1>', '<h1>Loon Check</h1>');
