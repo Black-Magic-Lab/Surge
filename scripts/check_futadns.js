8b663b17fe963d4a9b512e4c12ad868133a0bad5
Hiraku <dev@hiraku.tw>
2023-06-07 18:03:21 +0800

    update futadns check script

--- scripts/check_futadns.js
+++ scripts/check_futadns.js
@@ -14,18 +14,32 @@ const successMessage = {
   'icon-color': '#1FCFB4',
 };
 
-if (!v4.primaryAddress && !v6.primaryAddress) {
-  errorMessage.content = '\n錯誤：未連上網路';
-  $done(errorMessage);
-} else {
-  $httpClient.get('https://check.futa.gg', function (error, response, data) {
-    if (error) {
-      errorMessage.content += '\n錯誤：' + error;
-      $done(errorMessage);
-    }
-    if (data.includes('正在正確地運作')) {
-      $done(successMessage);
-    }
+(() => {
+  if (!v4.primaryAddress && !v6.primaryAddress) {
+    errorMessage.content = '\n錯誤：未連上網路';
     $done(errorMessage);
-  });
-}
+  } else {
+    $httpClient.get('https://check.futa.gg', function (error, response, data) {
+      if (error) {
+        errorMessage.content += `\n錯誤： ${error}`
+        $done(errorMessage);
+      }
+      if (response.status == 200) {
+        if (data) {
+          if (data.includes('正在正確地運作')) {
+            $done(successMessage);
+          } else {
+            $done(errorMessage);
+          }
+        } else {
+          errorMessage += `\n錯誤：無法連接到檢查頁面`
+          $done(errorMessage);
+        }
+      } else {
+        errorMessage.content += `\n錯誤：無法連接到檢查頁面 ${response.status}`
+      }
+      $done(errorMessage);
+    });
+  }
+})()
+
